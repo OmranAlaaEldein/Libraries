a08921c6721c2912d41fc06f32e040e039929bf0
OmranAlaaEldein <omranalaaeldein@gmail.com>
2022-04-16 02:45:58 +0300

    CreateDB

--- Libraries/Models/LibrariesDBContext.cs
+++ Libraries/Models/LibrariesDBContext.cs
@@ -7,13 +7,36 @@ using System.Threading.Tasks;
 namespace Libraries.Models
 {
     public class LibrariesDBContext : DbContext
-    {  
+    {
+        public DbSet<Library> Libraries { get; set; }
+        public DbSet<Book> Books { get; set; }
+
         public LibrariesDBContext(DbContextOptions<LibrariesDBContext> options) : base(options)
         {
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-             }
+            // Configure Library  
+            modelBuilder.Entity<Library>(b => {
+                b.HasKey(p => p.Id);
+                b.Property(p => p.Name).IsRequired().HasMaxLength(20);
+                b.HasMany<Book>(p => p.Books).WithOne(x => x.Library).OnDelete(DeleteBehavior.Cascade);
+            });
+
+            // Configure Book  
+            modelBuilder.Entity<Book>(b => {
+                b.HasKey(p => p.Id);
+                b.Property(p => p.Tittle).IsRequired().HasMaxLength(20);
+            });
+
+            // Configure Author  
+            modelBuilder.Entity<Author>(b => {
+                b.HasKey(p => p.Id);
+                b.Property(p => p.Name).IsRequired().HasMaxLength(20);
+                b.Property(p => p.LastName).IsRequired().HasMaxLength(20);
+                b.HasMany<Book>(p => p.Books).WithOne(x => x.Author).OnDelete(DeleteBehavior.Cascade);
+            });
+        }
     }
 }
